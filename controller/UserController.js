f81d04225c4fea3cc526c16f44f57646a953b76e
choikc1999 <choikc1999@naver.com>
2023-08-07 18:29:40 +0900

    Update

--- controller/UserController.js
+++ controller/UserController.js
@@ -7,28 +7,28 @@ exports.index = (req, res) => {
 
 exports.checkDuplicateId = (req, res) => {
     const { id } = req.body;
-    User.checkDuplicateId(id, function(err, isDuplicate){
+    User.checkDuplicateId(id, function (err, isDuplicate) {
         if (err) {
             console.error("Error checking duplicate ID", err);
-            return res.status(500).json({error: "Error checking duplicate ID"});
+            return res.status(500).json({ error: "Error checking duplicate ID" });
         }
-        res.json({isDuplicate: isDuplicate});
+        res.json({ isDuplicate: isDuplicate });
     });
 };
 
 exports.check_duplicate_id = (req, res) => {
     const id = req.body.id;
 
-    User.checkDuplicateId(id, function(err,isDuplicate){
-        if(err){
+    User.checkDuplicateId(id, function (err, isDuplicate) {
+        if (err) {
             console.error("Error checking duplicate ID in MySQL", err);
-            return res.status(500).json({error: "Error checking duplicate ID in the database"});
+            return res.status(500).json({ error: "Error checking duplicate ID in the database" });
         }
 
-        if(isDuplicate){
-            return res.status(200).json({isDuplicate: true});
-        }else{
-            return res.status(200).json({isDuplicate:false});
+        if (isDuplicate) {
+            return res.status(200).json({ isDuplicate: true });
+        } else {
+            return res.status(200).json({ isDuplicate: false });
         }
     });
 };
@@ -37,15 +37,16 @@ exports.check_duplicate_id = (req, res) => {
 exports.post_user = (req, res) => {
     const userData = req.body;
 
-    User.insert(userData, function(err, result){
-        if(err){
+    User.insert(userData, function (err, result) {
+        if (err) {
             console.error("UserController - Error inserting user data", err);
-            if(err === "Duplicate ID") {
-                return res.status(400).json({error: "Duplicate ID. please choose a different ID"});
-            }else{
-                console.error(500).json({error: "Error saving user data to the database"});
+            if (err === "Duplicate ID") {
+                return res.status(400).json({ error: "Duplicate ID. Please choose a different ID." });
+            } else {
+                console.error("Error saving user data to MySQL", err);
+                return res.status(500).json({ error: "Error saving user data to the database" });
             }
         }
-        return res.status(200).json({id:result.id});
+        return res.status(200).json({ id: result.id });
     });
 };
--- model/User.js
+++ model/User.js
@@ -7,14 +7,15 @@ const cnn = mysql.createConnection({
     password: 'choikc0901!',
     database: 'portfolio',
     port: 3306,
+    
 });
 
 // 아이디 중복 확인
 exports.checkDuplicateId = (id, cb) => {
-    let sql = `SELECT id FROM user WHERE id = '${id}' LIMIT 1;`;
+    let sql = `SELECT id From user WHERE id = '${id}' LIMIT 1;`;
 
-    cnn.query(sql, function(err, rows){
-        if(err){
+    cnn.query(sql, function (err, rows){
+        if(err) {
             console.error("Error executing MySQL query", err);
             return cb(err);
         }
@@ -24,23 +25,23 @@ exports.checkDuplicateId = (id, cb) => {
 
 // 회원가입 정복 입력
 exports.insert = (data, cb) => {
-    exports.checkDuplicateId(data.id, function (err, isDuplicate){
+    exports.checkDuplicateId(data.id, function (err, isDuplicate) {
         if(err){
             cb(err);
-        }else if(isDuplicate){
+        }else if (isDuplicate){
             cb("Duplicate ID");
         }else{
-            var sql = `INSERT INTO user VALUES ('${data.id}', '${data.name}', '${data.email}', '${data.phoneNumber}','${data.password}';)`;
+            var sql = `INSERT INTO user VALUES ('${data.id}','${data.name}','${data.email}','${data.phoneNumber}','${data.password}');`;
 
             cnn.query(sql, (err, rows) => {
-                if(err){
-                    console.error("Error executing MySQL query", err);
-                    return cb(err);
-                }
-                cb(null, {id: data.id});
+            if(err) {
+                console.error("Error executing MySQL query", err);
+                return cb(err);
+            }
+            cb(null,{id: data.id});
             });
         }
-    }); 
+    });
 }
 
 // throw = 사용자가 정의한 '예외'를 발생시킬 수 있다, '예외'가 발생하면 함수가 중지되고 catch문으로 전달된다. catch문이 없다면 프로그램이 종료된다.
@@ -50,27 +51,27 @@ exports.insert = (data, cb) => {
 // 로그인 정보 읽기
 exports.select = (id, password, cb) => {
     var sql = `SELECT * FROM user WHERE id = '${id}'limit 1`;
-
+    
     cnn.query(sql, (err, rows) => {
-        if(err) throw err;
+        if (err) throw err;
         cb(rows[0]);
     });
 }
 
 // 회원정보
 exports.get_user = (id, cb) => {
-    let sql = `SELECT * FROM user WHERE id = '${id}' limit 1`;
+    let sql = `SELECT * FROM user WHERE id = '${id}' limit 1;`;
 
     cnn.query(sql, function(err, rows){
-        if (err) throw err;
+        if(err) throw err;
         cb(rows);
     });
 }
 
 // 회원 정보 수정
 exports.update = (data, cb) => {
-    var sql = `UPDATE user SET name = '${data.name}', email = '${data.email}', phoneNumber = '${data.phoneNumber}', password = '${data.password}' WHERE id = '${data.id}';`;
-    
+    var sql = `UPDATE user SET name = '${data.name}', email = '${data.email}' phoneNumber = '${data.phoneNumber}', password = '${data.password}', WHERE id = '${data.id}';`;
+
     cnn.query(sql, (err, rows) => {
         if(err) throw err;
         cb(rows);
