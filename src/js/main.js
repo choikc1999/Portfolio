f042674dbca54221f03d48d0a8ddd2356ac9d2f0
choikc1999 <choikc1999@naver.com>
2023-08-20 20:51:32 +0900

    Update main

--- src/js/main.js
+++ src/js/main.js
@@ -91,4 +91,138 @@ $(document).ready(function() {
             });
         });
     }
+
+    // 슬라이드
+    var $slides = $('.slide'); // 각 슬라이드
+    var $slidesContainer = $('.slides'); // 슬라이더 컨테이너
+    
+    // 원본 슬라이드를 복제하여 슬라이드를 더 추가 (원하는 개수만큼)
+    for (var i = 0; i < 100; i++) {
+        $slides.clone().appendTo('.slides');
+    }
+    
+    var $allSlides = $('.slide'); // 모든 슬라이드 (원본 및 복제된)
+    var slideCount = $allSlides.length; // 슬라이드 개수
+    
+    $slidesContainer.css('width', '100%'); // 슬라이더 컨테이너의 너비를 100%로 설정
+    $slidesContainer.css('overflow', 'hidden'); // 너비를 벗어나는 내용 감춤
+    
+    $allSlides.css('margin-right', '10px'); // 모든 슬라이드의 여백 설정
+    $allSlides.css('width', '100vw'); // 모든 슬라이드의 너비를 창의 너비만큼으로 설정
+    
+    var slideWidth = $allSlides.width(); // 각 슬라이드의 너비
+    var currentIndex = 0; // 현재 슬라이드 인덱스
+    
+    $slidesContainer.css('width', slideWidth * slideCount + 'px'); // 슬라이더 컨테이너의 너비 설정
+    
+    // 각 슬라이드를 부드럽게 이동하는 함수
+    function moveSlides() {
+        var position = -1 * currentIndex * slideWidth;
+        $slidesContainer.css({
+            'transform': 'translateX(' + position + 'px)',
+            'transition': 'transform 0.5s ease-in-out' // 부드러운 애니메이션 적용
+        });
+    }
+    
+    // 자동 슬라이드를 시작하는 함수
+    function startSliderInterval() {
+        interval = setInterval(function() {
+            currentIndex++;
+            if (currentIndex >= slideCount) {
+                currentIndex = 0;
+            }
+            moveSlides();
+        }, 2000); // 2초 간격으로 슬라이드 이동
+    }
+    
+    var touchStartX = 0;
+    var touchEndX = 0;
+    var touchThreshold = 100; // 터치 이동 감지 임계값
+    var interval = startSliderInterval(); // 초기 슬라이드 이동 시작
+    
+    // 슬라이더 컨테이너에 터치 및 마우스 이벤트 추가
+    $('.portfolio_slider').on({
+        mouseenter: function() {
+            clearInterval(interval); // 슬라이더 이동 멈춤
+            interval = null; // interval을 초기화하여 중복 실행 방지
+        },
+        mouseleave: function() {
+            if (!interval) { // interval이 없을 때만 다시 시작
+                interval = startSliderInterval(); // 슬라이더 이동 다시 시작
+            }
+        },
+        touchstart: function(e) {
+            touchStartX = e.touches[0].clientX;
+        },
+        touchmove: function(e) {
+            touchEndX = e.touches[0].clientX;
+        },
+        touchend: function() {
+            var touchDiff = touchStartX - touchEndX;
+    
+            if (touchDiff > touchThreshold) {
+                currentIndex++;
+            } else if (touchDiff < -touchThreshold) {
+                currentIndex--;
+            }
+    
+            if (currentIndex < 0) {
+                currentIndex = slideCount - 1;
+            } else if (currentIndex >= slideCount) {
+                currentIndex = 0;
+            }
+    
+            moveSlides();
+    
+            touchStartX = 0;
+            touchEndX = 0;
+        },
+        mousedown: function(e) {
+            touchStartX = e.clientX;
+            $(this).on('mousemove', function(e) {
+                touchEndX = e.clientX;
+            });
+            $(this).on('mouseup', function() {
+                var touchDiff = touchStartX - touchEndX;
+    
+                if (touchDiff > touchThreshold) {
+                    currentIndex++;
+                } else if (touchDiff < -touchThreshold) {
+                    currentIndex--;
+                }
+    
+                if (currentIndex < 0) {
+                    currentIndex = slideCount - 1;
+                } else if (currentIndex >= slideCount) {
+                    currentIndex = 0;
+                }
+    
+                moveSlides();
+    
+                $(this).off('mousemove mouseup');
+                touchStartX = 0;
+                touchEndX = 0;
+            });
+        }
+    });
+    
+    // 자동 슬라이드를 시작하는 함수
+    function startSliderInterval() {
+        return setInterval(function() {
+            currentIndex++;
+            if (currentIndex >= slideCount) {
+                currentIndex = 0;
+            }
+            moveSlides();
+        }, 2000); // 2초 간격으로 슬라이드 이동
+    }
+    
+    // 각 슬라이드를 부드럽게 이동하는 함수
+    function moveSlides() {
+        var position = -1 * currentIndex * slideWidth;
+        $slidesContainer.css({
+            'transform': 'translateX(' + position + 'px)',
+            'transition': 'transform 0.5s ease-in-out' // 부드러운 애니메이션 적용
+        });
+    }
 });
