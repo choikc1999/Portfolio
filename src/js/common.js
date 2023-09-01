b810b2b7de91888b88f8390b79eb554b8fc58fb7
choikc1999 <choikc1999@naver.com>
2023-09-01 16:42:19 +0900

    Update

--- src/js/common.js
+++ src/js/common.js
@@ -1,37 +1,47 @@
-$(document).ready(function(){
-    $(".searchicon").click(function () {
-        $(".hover").css("display", "none");
-        $(".searchicon").addClass("searchIconClickoff");
+$(document).ready(function() {
 
-        const searchIcon = document.querySelector('.searchicon');
-
-        searchIcon.addEventListener('animationend', () => {
-            searchIcon.style.display = 'none';
-            $(".c_1").css("display", "none");
-            $(".btn_search").addClass("searchIconClickon");
-            $(".c_2").css("display","block");
-            $(".inputback").css("display","inline-block");
-            
-            const searchIconClickon = document.querySelector('.searchIconClickon');
-
-            // 애니메이션 완료 후 처리
-            searchIconClickon.addEventListener('animationend', () => {
-                searchIconClickon.style.opacity = 1;
-                searchIconClickon.style.transform = 'scale(1)'; // 확대 애니메이션 완료 후 크기 복원
+    // 서치아이콘 효과부여
+    $('.circle').hover(function() {
+        var $searchicon = $(this).find('.searchicon');
+        setTimeout(function() {
+            $searchicon.css({
+                'z-index': '2',
+                'cursor': 'pointer'
             });
-            searchIconClickon.addEventListener("click", function() {
-                var inputValue = document.querySelector(".search_input").value;
-                
-                if (inputValue === "") {
-                    $(".c_2").css("display", "none");
-                    $(".c_1").css("display", "block");
-                    $(".searchicon").removeClass("searchIconClickoff").css("display","block")
-                    console.log("검색어를 입력하세요");
-                } else {
-                    // 입력값이 있는 경우의 동작을 여기에 구현
-                    console.log("검색 실행: " + inputValue);
-                }
-            });
-        });
+      }, 100); // 0.1초 (100ms) 지연 실행
+    }, function() {
+        var $searchicon = $(this).find('.searchicon');
+
+            $searchicon.css({
+                'z-index': 'auto',
+                'cursor': 'default'
+            }); 
+    });
+
+    $('.searchicon, .searchiconShow').click(function(){
+        if ($(this).hasClass("searchiconShow")) {
+            // 이미 "searchiconShow" 클래스가 있는 경우 처리
+            $('.searchiconShow').addClass("searchicon");
+            $(this).removeClass("searchiconShow");
+            $('.circle_click').addClass('circle').css({"width": "65px","transition": "0.5s"});
+            $('.circle_click').removeClass('circle_click');
+        } else {
+            // "searchiconShow" 클래스가 없는 경우 처리
+            var inputValue = document.querySelector(".search_input").value;
+
+            if (inputValue === "") {
+                $('.circle').addClass('circle_click');
+                $('.circle_click').removeClass('circle').css({"width": "400px", "transition": "0.5s"});
+                $(this).addClass("searchiconShow");
+                $('.searchiconShow').removeClass("searchicon");
+                $('.searchiconShow').attr('type', 'button'); // 버튼 타입을 "button"으로 변경
+                console.log("검색어를 입력하세요");
+            } else {
+                // 입력값이 있는 경우의 동작을 여기에 구현
+                $('.searchiconShow').attr('type', 'submit');// 버튼 타입을 "submit"으로 변경
+                alert("gg");
+                console.log("검색 실행: " + inputValue);
+            }
+        }
     });
 });
