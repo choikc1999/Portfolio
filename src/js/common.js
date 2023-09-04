e87dcd9503b82b96f66553d23849508559fe1d31
choikc1999 <choikc1999@naver.com>
2023-09-04 23:31:48 +0900

    update

--- src/js/common.js
+++ src/js/common.js
@@ -110,4 +110,68 @@ $(document).on('click', '.searchResult', function () {
     window.location.href = `/boardview?boardID=${boardID}`; // 해당 게시글의 페이지로 이동합니다.
 });
 
+// plus btn
+if (document.body.animate) {
+    document
+    .querySelectorAll(".Plus")
+    .forEach((Plus) => Plus.addEventListener("click", pop));
+}
+
+function pop(e) {
+    for (let i = 0; i < 30; i++) {
+    createParticle(e.clientX, e.clientY, e.target.dataset.type);
+    }
+}
+
+function createParticle(x, y, type) {
+    const particle = document.createElement("particle");
+    document.body.appendChild(particle);
+
+    const size = Math.floor(Math.random() * 20 + 5);
+
+    particle.style.width = `${size}px`;
+    particle.style.height = `${size}px`;
+
+    const destinationX = x + (Math.random() - 0.5) * 2 * 75;
+    const destinationY = y + (Math.random() - 0.5) * 2 * 75;
+
+    switch (type) {
+    case "circle":
+        particle.style.background = `hsl(${Math.random() * 88 + 53}, 70%, 60%)`;
+        particle.style.borderRadius = "50%";
+        break;
+    default:
+        particle.style.background = `hsl(${Math.random() * 86 + 56}, 70%, 60%)`;
+    }
+
+    const animation = particle.animate(
+    [
+        {
+        // Set the origin position of the particle
+        // We offset the particle with half its size to center it around the mouse
+        transform: `translate(${x - size / 2}px, ${y - size / 2}px)`,
+        opacity: 1,
+        },
+        {
+        // We define the final coordinates as the second keyframe
+        transform: `translate(${destinationX}px, ${destinationY}px)`,
+        opacity: 0,
+        },
+    ],
+    {
+        duration: 500 + Math.random() * 1000,
+        easing: "cubic-bezier(0, .9, .57, 1)",
+        delay: Math.random() * 200,
+    }
+    );
+
+    animation.onfinish = () => {
+    particle.removeParticle;
+    };
+}
+
+function removeParticle(e) {
+    e.srcElement.effect.target.remove();
+}
+
 });
