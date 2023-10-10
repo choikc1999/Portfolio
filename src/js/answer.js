31ff9d53b608b722a5df93e1fbf6fd83ed052775
choikc1999 <choikc1999@naver.com>
2023-10-10 23:10:30 +0900

    update

--- src/js/answer.js
+++ src/js/answer.js
@@ -1,11 +1,42 @@
-$.ajax({
-    method: 'GET',
-    url: '/get-user-info', // 사용자 정보를 가져오는 경로
-    success: function (response) {
-        // 사용자 정보에서 이름 가져와서 표시
-        document.getElementById("m_userName").textContent = response.name;
-    },
-    error: function (error) {
-        console.error("Error:", error);
-    }
+$(document).ready(function(){
+    $.ajax({
+        method: 'GET',
+        url: '/get-user-info', // 사용자 정보를 가져오는 경로
+        success: function (response) {
+            // 사용자 정보에서 이름 가져와서 표시
+            document.getElementById("userName").textContent = response.name;
+        },
+        error: function (error) {
+            console.error("Error:", error);
+        }
+    });
+
+    $.ajax({
+        method: 'GET',
+        url: '/get-user-info', // 사용자 정보를 가져오는 경로
+        success: function (response) {
+            // 사용자 정보에서 이름 가져와서 표시
+            document.getElementById("m_userName").textContent = response.name;
+        },
+        error: function (error) {
+            console.error("Error:", error);
+        }
+    });
+
+    // 로그아웃 버튼 클릭 이벤트 처리
+    document.querySelector(".signout").addEventListener("click", function () {
+        // 로그아웃 요청 보내기
+        $.ajax({
+            method: 'GET',
+            url: '/logout',
+            success: function (response) {
+                // 로그아웃 후 로그인 페이지로 리디렉션
+                window.location.href = "/login";
+            },
+            error: function (error) {
+                console.error("Error:", error);
+            }
+        });
+    });
+
 });
--- src/js/introduce.js
+++ src/js/introduce.js
@@ -1,4 +1,44 @@
 $(document).ready(function () {
+    $.ajax({
+        method: 'GET',
+        url: '/get-user-info', // 사용자 정보를 가져오는 경로
+        success: function (response) {
+            // 사용자 정보에서 이름 가져와서 표시
+            document.getElementById("userName").textContent = response.name;
+        },
+        error: function (error) {
+            console.error("Error:", error);
+        }
+    });
+
+    $.ajax({
+        method: 'GET',
+        url: '/get-user-info', // 사용자 정보를 가져오는 경로
+        success: function (response) {
+            // 사용자 정보에서 이름 가져와서 표시
+            document.getElementById("m_userName").textContent = response.name;
+        },
+        error: function (error) {
+            console.error("Error:", error);
+        }
+    });
+
+    // 로그아웃 버튼 클릭 이벤트 처리
+    document.querySelector(".signout").addEventListener("click", function () {
+        // 로그아웃 요청 보내기
+        $.ajax({
+            method: 'GET',
+            url: '/logout',
+            success: function (response) {
+                // 로그아웃 후 로그인 페이지로 리디렉션
+                window.location.href = "/login";
+            },
+            error: function (error) {
+                console.error("Error:", error);
+            }
+        });
+    });
+
     var text1 = "안녕하세요"; // 첫 번째 텍스트
     var index1 = 0; // 첫 번째 텍스트의 현재 인덱스
 
