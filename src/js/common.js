417e1fca265d32ddcd8cd772bf83f33be23b4f7d
choikc1999 <choikc1999@naver.com>
2023-10-11 14:57:14 +0900

    Update

--- src/js/common.js
+++ src/js/common.js
@@ -28,30 +28,30 @@ $(document).ready(function() {
             // 이미 "searchiconShow" 클래스가 있는 경우 처리
             $('.searchiconShow').addClass("searchicon");
             $(this).removeClass("searchiconShow");
+            $('.searchicon').css('filter', 'drop-shadow(0.2rem 0.2rem 0.2rem #0a0a0a)');
             $('.circle_click').addClass('circle').css({"width": "65px","transition": "0.5s"});
             $('.circle_click').removeClass('circle_click');
-        } else {
+        };
             // "searchiconShow" 클래스가 없는 경우 처리
-            var inputValue = document.querySelector(".search_input").value;
+        var inputValue = document.querySelector(".search_input").value;
+        $('.circle').addClass('circle_click');
+        $('.circle_click').removeClass('circle').css({"width": "300px", "transition": "0.5s"});
 
-            if (inputValue === "") {
-                $('.circle').addClass('circle_click');
-                $('.circle_click').removeClass('circle').css({"width": "300px", "transition": "0.5s"});
-                $(this).addClass("searchiconShow");
-                $('.searchiconShow').removeClass("searchicon");
-                $('.searchiconShow').attr('type', 'button'); // 버튼 타입을 "button"으로 변경
-                $('.circle_click').css('background','none');
-                $('.searchiconShow').css('filter', 'none');
-                console.log("검색어를 입력하세요");
-            } else {
-                // 입력값이 있는 경우의 동작을 여기에 구현
-                $('.searchiconShow').attr('type', 'submit');// 버튼 타입을 "submit"으로 변경
-                const searchTerm = $(".search_input").val(); // 검색어 가져오기
-                window.location.href = `/answer?searchTerm=${searchTerm}`;
-                // 검색을 실행하고 결과를 표시하는 부분
-                searchPosts(searchTerm); // 검색을 실행하도록 추가
-                console.log("검색 실행: " + inputValue);
-            }
+        if (inputValue === "") {
+            $(this).addClass("searchiconShow");
+            $('.searchiconShow').removeClass("searchicon");
+            $('.searchiconShow').attr('type', 'button'); // 버튼 타입을 "button"으로 변경
+            $('.circle_click').css('background','none');
+            $('.searchiconShow').css('filter', 'none');
+            console.log("검색어를 입력하세요");
+        } else {
+            // 입력값이 있는 경우의 동작을 여기에 구현
+            $('.searchiconShow').attr('type', 'submit');// 버튼 타입을 "submit"으로 변경
+            const searchTerm = $(".search_input").val(); // 검색어 가져오기
+            searchPosts(searchTerm); // 검색을 실행하도록 추가
+            window.location.href = `/answer?searchTerm=${searchTerm}`;
+            // 검색을 실행하고 결과를 표시하는 부분
+            console.log("검색 실행: " + inputValue);
         }
     });
 
